98d13c06ab1b36bbc430234c8cce50716964cfb5
SahibSanai <sahibsanai@gmail.com>
2026-05-04 19:35:23 +0400

    Fix build problem

--- Epoint/Epoint/Models/EpointSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Epoint.Models;
-
-public class EpointSettings
-{
-    public string PublicKey { get; set; } = string.Empty;
-    public string PrivateKey { get; set; } = string.Empty;
-    public string BaseUrl { get; set; } = "https://epoint.az/api/1";
-}
